1a19a4768bacdab2f0818fe6f80649cbeb746ae1
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-08-21 19:09:11 +0100

    feat: create BoxInfo component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import MyButton from '@/components/button/Button.vue'
+import Button from '@/components/button/Button.vue'
+import Box from './components/box/Box.vue'
 
+// Função de exemplo para lidar com o clique do botão
 const handleButtonClick = () => {
   console.log('Botão foi clicado!')
 }
@@ -8,25 +10,35 @@ const handleButtonClick = () => {
 
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-md">
-        <div class="card">
-          <div class="card-header">
-            <h5 class="card-title">Usando Button</h5>
-          </div>
-          <div class="card-body text-center">
-            <MyButton
-              type="warning"
-              size="btn-lg"
-              :isDisabled="false"
-              icon="fas fa-edit"
-              @click="handleButtonClick"
-            >
-              Editar
-            </MyButton>
-          </div>
+    <!-- Card para o componente Button -->
+    <div class="col-md">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Usando Button</h5>
+        </div>
+        <div class="card-body text-center">
+          <Button
+            type="warning"
+            size="btn-lg"
+            :isDisabled="false"
+            icon="fas fa-edit"
+            @click="handleButtonClick"
+          >
+            Editar
+          </Button>
         </div>
       </div>
+    </div>
+
+    <!-- Card para o componente Box -->
+    <div class="small-box bg-success">
+      <Box
+        title="53"
+        subtitle="%"
+        description="Bounce Rate"
+        icon="ion ion-stats-bars"
+        boxType="small-box bg-info"
+      />
     </div>
   </div>
 </template>
---
+++ src/components/box/Box.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="['small-box', boxType]">
+    <div class="inner">
+      <h3>
+        {{ title }}<sup v-if="subtitle" style="font-size: 20px">{{ subtitle }}</sup>
+      </h3>
+      <p>{{ description }}</p>
+    </div>
+    <div class="icon">
+      <i :class="icon"></i>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Title'
+  },
+  subtitle: {
+    type: String,
+    default: ''
+  },
+  description: {
+    type: String,
+    default: 'Description'
+  },
+  icon: {
+    type: String,
+    default: 'ion ion-stats-bars'
+  },
+  boxType: {
+    type: String,
+    default: 'bg-success'
+  }
+})
+</script>
