55f846bec038c792ad4c6b81e35fc0b42252b64c
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-08-26 17:13:36 +0100

    refactory: removing items that are not being used

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -16,7 +16,6 @@
             </div>
           </div>
         </div>
-        <!-- /.card-header -->
         <div class="card-body table-responsive p-0">
           <table class="table table-hover text-nowrap">
             <thead>
@@ -25,15 +24,13 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(row, index) in filteredData" :key="index">
+              <tr v-for="(row, index) in data" :key="index">
                 <td v-for="column in columns" :key="column.key">{{ row[column.key] }}</td>
               </tr>
             </tbody>
           </table>
         </div>
-        <!-- /.card-body -->
       </div>
-      <!-- /.card -->
     </div>
   </div>
 </template>
@@ -42,7 +39,6 @@
 import { ref, computed, defineProps } from 'vue'
 import SearchInput from '../search-input/search-input.vue'
 
-// Props
 const props = defineProps({
   title: {
     type: String,
@@ -59,51 +55,33 @@ const props = defineProps({
   search: {
     type: Boolean,
     default: true
-  },
-  searchPlaceholder: {
-    type: String,
-    default: 'Search...'
   }
 })
 
 const selectedColumn = ref(props.columns[0].key)
-const searchQuery = ref('')
-
-// Filtered data
-const filteredData = computed(() => {
-  if (!searchQuery.value) return props.data
-  return props.data.filter((row) => {
-    return String(row[selectedColumn.value]).toLowerCase().includes(searchQuery.value.toLowerCase())
-  })
-})
-
-// Methods
-const onSearch = () => {
-  // Ação de busca se necessário
-}
 </script>
 
 <style scoped>
 .optionbox {
   display: flex;
   align-items: center;
-  gap: 0.5rem; /* Espaço entre o select e o campo de pesquisa */
+  gap: 0.5rem;
 }
 
 .optionbox select {
-  background: #ffffff; /* Cor branca para o fundo */
-  color: #000000; /* Texto preto para contraste */
-  padding: 5px; /* Reduzido o padding */
-  width: 150px; /* Largura menor */
-  height: 35px; /* Altura menor */
-  border: 1px solid #ccc; /* Borda cinza clara */
-  font-size: 14px; /* Tamanho da fonte reduzido */
-  box-shadow: none; /* Removido o box-shadow */
+  background: #ffffff;
+  color: #000000;
+  padding: 5px;
+  width: 150px;
+  height: 35px;
+  border: 1px solid #ccc;
+  font-size: 14px;
+  box-shadow: none;
   outline: none;
 }
 
 .optionbox select:focus {
-  border-color: #007bff; /* Cor de foco padrão do Bootstrap */
-  box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.25); /* Sombra leve de foco */
+  border-color: #007bff;
+  box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.25);
 }
 </style>
